21f0f50f52266cfa29327039d04cfde9c08533fc
Jonas Holst Damtoft <jonasdamtoft@gmail.com>
2021-05-03 14:37:59 +0200

    refactor(Map): isolate go to

--- src/map/MapMenu.tsx
+++ src/map/MapMenu.tsx
@@ -2,13 +2,13 @@ import {
   ListSubheader,
   Menu,
   MenuItem,
+  MenuItemProps,
   PopoverPosition,
 } from '@material-ui/core';
 import React from 'react';
 import {
   Capability,
   RawMapLayerMetaData,
-  RobotState,
   useGoToMutation,
   useRobotState,
 } from '../api';
@@ -28,29 +28,49 @@ const DefaultAnchorPosition: PopoverPosition = {
   left: 0,
 };
 
-const gotoEnabledStates = new Set<RobotState['status']['state']>([
-  'idle',
-  'docked',
-]);
+interface GoToItemProps extends MenuItemProps {
+  position?: Coordinates;
+}
+const GoToItem = (props: GoToItemProps): JSX.Element | null => {
+  const { onClick, position, ...rest } = props;
+  const { data: status } = useRobotState((state) => state.status.state);
+  const { mutate: sendGoTo } = useGoToMutation();
+
+  const handleGoTo = React.useCallback<React.MouseEventHandler<HTMLLIElement>>(
+    (event) => {
+      onClick?.(event);
+      if (position === undefined) {
+        // _Should_ never end up here.
+        return;
+      }
+
+      sendGoTo(position);
+    },
+    [onClick, position, sendGoTo]
+  );
+
+  if (position === undefined || status === undefined) {
+    return null;
+  }
+
+  if (status !== 'idle' && status !== 'docked') {
+    return null;
+  }
+
+  return (
+    <MenuItem {...rest} button onClick={handleGoTo}>
+      Go here
+    </MenuItem>
+  );
+};
+
 const MapMenu = (props: MapMenuProps): JSX.Element => {
   const { anchorPosition, open, onClose, segment, position } = props;
-  const { data: status } = useRobotState((state) => state.status.state);
-  const [isGoToSupported] = useCapabilitiesSupported(Capability.GoToLocation);
-  const { mutate: sendGoTo } = useGoToMutation();
+  const [goto] = useCapabilitiesSupported(Capability.GoToLocation);
   const roundedArea =
     segment?.area !== undefined
       ? ((segment?.area ?? 1) * 0.0001).toFixed(1)
       : '?';
-
-  const handleGoTo = React.useCallback(() => {
-    onClose();
-    if (position === undefined) {
-      // _Should_ never end up here.
-      return;
-    }
-
-    sendGoTo(position);
-  }, [onClose, position, sendGoTo]);
 
   return (
     <Menu
@@ -65,14 +85,15 @@ const MapMenu = (props: MapMenuProps): JSX.Element => {
           {`id: ${segment.segmentId}, area: ${roundedArea}m²`.trim()}
         </ListSubheader>
       )}
-      {position !== undefined &&
-        status !== undefined &&
-        gotoEnabledStates.has(status) &&
-        isGoToSupported && <MenuItem onClick={handleGoTo}>Go here</MenuItem>}
+      {goto && <GoToItem onClick={onClose} position={position} />}
       {segment !== undefined && (
-        <MenuItem onClick={onClose}>Add segment</MenuItem>
+        <MenuItem button onClick={onClose}>
+          Add segment
+        </MenuItem>
       )}
-      <MenuItem onClick={onClose}>Add zone</MenuItem>
+      <MenuItem button onClick={onClose}>
+        Add zone
+      </MenuItem>
     </Menu>
   );
 };
