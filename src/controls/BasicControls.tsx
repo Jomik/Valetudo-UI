57265fe9cdbfb2f7dfc03de324140655f3ba53e6
Jonas Holst Damtoft <jonasdamtoft@gmail.com>
2021-05-03 23:33:12 +0200

    feat(Controls): make basic controls group static

--- src/controls/BasicControls.tsx
+++ src/controls/BasicControls.tsx
@@ -1,6 +1,13 @@
-import { Box, Button, ButtonGroup, makeStyles, Paper } from '@material-ui/core';
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  makeStyles,
+  Paper,
+  Typography,
+} from '@material-ui/core';
 import React from 'react';
-import { useRobotStatus, useBasicControlMutation } from '../api';
+import { useRobotStatus, useBasicControlMutation, StatusState } from '../api';
 import {
   Home as HomeIcon,
   Pause as PauseIcon,
@@ -15,91 +22,57 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const StartStates: StatusState['value'][] = ['idle', 'docked', 'paused'];
+const PauseStates: StatusState['value'][] = ['cleaning', 'returning', 'moving'];
+
 const BasicControls = (): JSX.Element => {
   const classes = useStyles();
   const { data: status } = useRobotStatus();
-  const { mutate: sendCommand, isLoading } = useBasicControlMutation();
+  const { mutate, isLoading } = useBasicControlMutation();
+  const sendCommand = (...args: Parameters<typeof mutate>) => () =>
+    mutate(...args);
 
-  const actions = React.useMemo<
-    { name: string; icon: JSX.Element; onClick(): void }[]
-  >(() => {
-    if (status === undefined) {
-      return [];
-    }
-    const actions: { name: string; icon: JSX.Element; onClick(): void }[] = [];
-    const { value: state, flag } = status;
+  if (status === undefined) {
+    return (
+      <Paper>
+        <Box p={1}>
+          <Typography color="error">Error loading basic controls</Typography>
+        </Box>
+      </Paper>
+    );
+  }
 
-    if (state === 'error') {
-      return [];
-    }
-
-    if (flag === 'resumable') {
-      actions.push(
-        {
-          name: 'Resume',
-          icon: <StartIcon />,
-          onClick() {
-            sendCommand('start');
-          },
-        },
-        {
-          name: 'Stop',
-          icon: <StopIcon />,
-          onClick() {
-            sendCommand('stop');
-          },
-        }
-      );
-    } else if (state === 'idle' || state === 'docked') {
-      actions.push({
-        name: 'Start',
-        icon: <StartIcon />,
-        onClick() {
-          sendCommand('start');
-        },
-      });
-    } else {
-      actions.push({
-        name: 'Stop',
-        icon: <StopIcon />,
-        onClick() {
-          sendCommand('stop');
-        },
-      });
-    }
-
-    if (state === 'cleaning' || state === 'returning') {
-      actions.push({
-        name: 'Pause',
-        icon: <PauseIcon />,
-        onClick() {
-          sendCommand('pause');
-        },
-      });
-    }
-
-    if (state === 'idle') {
-      actions.push({
-        name: 'Home',
-        icon: <HomeIcon />,
-        onClick() {
-          sendCommand('home');
-        },
-      });
-    }
-
-    return actions;
-  }, [sendCommand, status]);
+  const { flag, value: state } = status;
 
   return (
     <Paper>
       <Box p={1}>
         <ButtonGroup size="medium" disabled={isLoading}>
-          {actions.map(({ name, icon, onClick }) => (
-            <Button key={name} onClick={onClick}>
-              {React.cloneElement(icon, { className: classes.icon })} {name}
-            </Button>
-          ))}
+          <Button
+            disabled={!StartStates.includes(state)}
+            onClick={sendCommand('start')}
+          >
+            <StartIcon className={classes.icon} />{' '}
+            {flag === 'resumable' ? 'Resume' : 'Start'}
+          </Button>
+          <Button
+            disabled={!PauseStates.includes(state)}
+            onClick={sendCommand('pause')}
+          >
+            <PauseIcon className={classes.icon} /> Pause
+          </Button>
+          <Button
+            disabled={
+              !(flag === 'resumable') &&
+              (state === 'idle' || state === 'docked')
+            }
+            onClick={sendCommand('stop')}
+          >
+            <StopIcon className={classes.icon} /> Stop
+          </Button>
+          <Button disabled={state !== 'idle'} onClick={sendCommand('home')}>
+            <HomeIcon className={classes.icon} /> Home
+          </Button>
         </ButtonGroup>
       </Box>
     </Paper>
