eb64afed9f8ec27b4b7e1ab51f3df93d9d1bef5d
Jonas Holst Damtoft <jonasdamtoft@gmail.com>
2021-05-04 17:02:51 +0200

    feat(Controls): remove redundant FAB

--- src/controls/ControlsBottomSheet.tsx
+++ src/controls/ControlsBottomSheet.tsx
@@ -1,23 +1,7 @@
-import {
-  Box,
-  CircularProgress,
-  Fab,
-  FabProps,
-  Grid,
-  makeStyles,
-  styled,
-  Typography,
-} from '@material-ui/core';
-import {
-  Home as HomeIcon,
-  PlayArrow as StartIcon,
-  Stop as StopIcon,
-} from '@material-ui/icons';
+import { Box, Grid, styled, Typography } from '@material-ui/core';
 import { BottomSheet } from 'react-spring-bottom-sheet';
 import ControlsBody from './ControlsBody';
 import 'react-spring-bottom-sheet/dist/style.css';
-import { Capability, useBasicControlMutation, useRobotStatus } from '../api';
-import { useCapabilitiesSupported } from '../CapabilitiesProvider';
 
 const StyledBottomSheet = styled(BottomSheet)(({ theme }) => ({
   '--rsbs-bg': theme.palette.background.paper,
@@ -32,84 +16,7 @@ const Sheet = styled(Box)(({ theme }) => ({
   backgroundColor: theme.palette.background.default,
 }));
 
-const StyledFab = styled(Fab)(({ theme }) => ({
-  position: 'absolute',
-  top: -17,
-  right: theme.spacing(5),
-}));
-
-const useActionButtonStyles = makeStyles((theme) => ({
-  extendedIcon: {
-    marginRight: theme.spacing(1),
-  },
-}));
-
-const fabProps: Partial<FabProps> = {
-  color: 'primary',
-  size: 'medium',
-  variant: 'extended',
-};
-const ActionButton = (): JSX.Element | null => {
-  const classes = useActionButtonStyles();
-  const { data: state } = useRobotStatus((status) => status.value);
-  const { mutate: sendCommand, isLoading } = useBasicControlMutation();
-
-  if (state === undefined) {
-    return null;
-  }
-
-  if (isLoading) {
-    return (
-      <StyledFab {...fabProps} variant="round">
-        <CircularProgress color="inherit" size={30} />
-      </StyledFab>
-    );
-  }
-
-  if (state === 'docked') {
-    return (
-      <StyledFab
-        {...fabProps}
-        onClick={() => {
-          sendCommand('start');
-        }}
-      >
-        <StartIcon className={classes.extendedIcon} />
-        Start
-      </StyledFab>
-    );
-  }
-
-  if (state === 'cleaning' || state === 'moving' || state === 'returning') {
-    return (
-      <StyledFab
-        {...fabProps}
-        onClick={() => {
-          sendCommand('stop');
-        }}
-      >
-        <StopIcon className={classes.extendedIcon} />
-        Stop
-      </StyledFab>
-    );
-  }
-
-  return (
-    <StyledFab
-      {...fabProps}
-      onClick={() => {
-        sendCommand('home');
-      }}
-    >
-      <HomeIcon className={classes.extendedIcon} />
-      Home
-    </StyledFab>
-  );
-};
-
 const ControlsBottomSheet = (): JSX.Element => {
-  const [basicControl] = useCapabilitiesSupported(Capability.BasicControl);
-
   return (
     <StyledBottomSheet
       open
@@ -122,7 +29,6 @@ const ControlsBottomSheet = (): JSX.Element => {
       ]}
       header={
         <>
-          {basicControl && <ActionButton />}
           <Grid container>
             <Grid item>
               <Typography variant="subtitle1">Controls</Typography>
